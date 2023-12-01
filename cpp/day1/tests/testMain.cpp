6e84d4a260922a0d98478c85e4196ac5601e55bb
xiaoming <mail@zyming.me>
2023-12-01 18:33:58 +0100

    Add parameterized unittests for problem2 of day1

--- cpp/day1/tests/testMain.cpp
+++ cpp/day1/tests/testMain.cpp
@@ -9,13 +9,7 @@ using input_t = std::string;
 using result_t = std::uint32_t;
 
 class AdventOfCodeDay1Problem1Test : public TestWithParam<std::pair<input_t, result_t>>
-{
-public:
-    AdventOfCodeDay1Problem1Test()
-    {
-
-    }
-};
+{};
 
 TEST_F(AdventOfCodeDay1Problem1Test, verify_sumOfNumberLines)
 {
@@ -29,7 +23,7 @@ TEST_P(AdventOfCodeDay1Problem1Test, unitTest_verifySolutionWithParams)
     EXPECT_EQ(result, reconstructCalibrationDocument(input));
 }
 
-INSTANTIATE_TEST_CASE_P(AdventOfCodeDay1Problem1,
+INSTANTIATE_TEST_CASE_P(AdventOfCodeDay1,
                         AdventOfCodeDay1Problem1Test,
                         Values(
                             std::make_pair("11", 11),
@@ -40,4 +34,25 @@ INSTANTIATE_TEST_CASE_P(AdventOfCodeDay1Problem1,
                             std::make_pair("1", 11),
                             std::make_pair("ab1c2\ngasd26", 38),
                             std::make_pair("ab1c2\ngasd2 6", 38)
+                        ));
+
+class AdventOfCodeDay1Problem2Test : public TestWithParam<std::pair<input_t, result_t>>
+{};
+
+TEST_F(AdventOfCodeDay1Problem2Test, DISABLED_verify_sumNumberLinesV2)
+{
+    constexpr auto input = "two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen";
+    EXPECT_EQ(281, reconstructCalibrationDocument(input));
+}
+
+TEST_P(AdventOfCodeDay1Problem2Test, unitTest_verifySolutionWithParams)
+{
+    const auto& [input, result] = GetParam();
+    EXPECT_EQ(result, reconstructCalibrationDocumentV2(input));
+}
+
+INSTANTIATE_TEST_CASE_P(AdventOfCodeDay1,
+                        AdventOfCodeDay1Problem2Test,
+                        Values(
+                            std::make_pair("onetwo",12)
                         ));
