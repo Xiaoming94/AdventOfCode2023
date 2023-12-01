f88435987e5a6991fc0cd01a002db657ee592e09
xiaoming <mail@zyming.me>
2023-12-01 18:21:45 +0100

    Parameterized all tests for problem 1

--- cpp/day1/tests/testMain.cpp
+++ cpp/day1/tests/testMain.cpp
@@ -32,59 +32,12 @@ TEST_P(AdventOfCodeDay1Problem1Test, unitTest_verifySolutionWithParams)
 INSTANTIATE_TEST_CASE_P(AdventOfCodeDay1Problem1,
                         AdventOfCodeDay1Problem1Test,
                         Values(
-                            std::make_pair("11",11)
+                            std::make_pair("11", 11),
+                            std::make_pair("1abc2", 12),
+                            std::make_pair("1a5c2", 12),
+                            std::make_pair("123456", 16),
+                            std::make_pair("a1c", 11),
+                            std::make_pair("1", 11),
+                            std::make_pair("ab1c2\ngasd26", 38),
+                            std::make_pair("ab1c2\ngasd2 6", 38)
                         ));
-
-TEST(AdventOfCodeDay1Test, verify_sumOfNumbersInLines)
-{
-    constexpr auto input = "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet";
-    EXPECT_EQ(142, reconstructCalibrationDocument(input));
-}
-
-TEST(AdventOfCodeDay1Test, verify_1Line)
-{
-    constexpr auto input{"11"};
-    EXPECT_EQ(11, reconstructCalibrationDocument(input));
-}
-
-TEST(AdventOfCodeDay1Test, verify_1LineWithChars)
-{
-    constexpr auto input{"1abc2"};
-    EXPECT_EQ(12, reconstructCalibrationDocument(input));
-}
-
-TEST(AdventOfCodeDay1Test, verify_1LineWithMultipleNumbers)
-{
-    constexpr auto input{"1a5c2"};
-    EXPECT_EQ(12, reconstructCalibrationDocument(input));
-}
-
-TEST(AdventOfCodeDay1Test, verify_1LineWithOnlyMultipleNumbers)
-{
-    constexpr auto input{"123456"};
-    EXPECT_EQ(16, reconstructCalibrationDocument(input));
-}
-
-TEST(AdventOfCodeDay1Test, verify_1LineWithOnly1Number)
-{
-    constexpr auto input{"a1c"};
-    EXPECT_EQ(11, reconstructCalibrationDocument(input));
-}
-
-TEST(AdventOfCodeDay1Test, verify_1LineOnly1Digit)
-{
-    constexpr auto input{"1"};
-    EXPECT_EQ(11, reconstructCalibrationDocument(input));
-}
-
-TEST(AdventOfCodeDay1Test, verify_2Line)
-{
-    constexpr auto input{"ab1c2\ngasd26"};
-    EXPECT_EQ(38, reconstructCalibrationDocument(input));
-}
-
-TEST(AdventOfCodeDay1Test, verify_2LineWithSpace)
-{
-    constexpr auto input{"ab1c2\ngasd2 6"};
-    EXPECT_EQ(38, reconstructCalibrationDocument(input));
-}
