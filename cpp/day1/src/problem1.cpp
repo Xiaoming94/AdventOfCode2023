c7715144dbf9248653b0d7694a1aba994a768da4
Xiaoming <mail@zyming.me>
2023-12-01 15:43:58 +0100

    Add function  for using lines with requirement

--- cpp/day1/src/problem1.cpp
+++ cpp/day1/src/problem1.cpp
@@ -1,6 +1,68 @@
 #include "problem1.h"
+#include <optional>
+#include <vector>
+#include <numeric>
+#include <sstream>
+
+namespace{
+    std::optional<std::uint8_t> convertToNumber(const char character)
+    {
+        if(std::isdigit(character))
+        {
+            return character - '0';
+        }
+        else
+        {
+            return std::nullopt;
+        }
+    }
+
+    bool isNotAssigned(std::uint32_t variable)
+    {
+        return variable > 9;
+    }
+
+    std::uint32_t reconstructCalibrationDocumentLine(const std::string& input)
+    {
+        auto first = 10u;
+        auto last = 10u;
+        for (const auto character : input)
+        {
+            if (const auto number = convertToNumber(character))
+            {
+                last = *number;
+                if (isNotAssigned(first))
+                {
+                    first = *number;
+                }
+            }
+        }
+        return first * 10 + last;
+    }
+
+    std::vector<std::string> splitLines(const std::string& input)
+    {
+        std::vector<std::string> lines;
+        std::stringstream inputStream(input);
+        std::string line;
+        while(std::getline(inputStream, line, '\n'))
+        {
+            lines.push_back(line);
+        }
+        return lines;
+    }
+}
+
 
 std::uint32_t reconstructCalibrationDocument(const std::string& input)
 {
-    return 0;
+    //split lines of Input
+    //For each lines : reconstructCalibrationDocumentLine(line)
+    const std::vector<std::string> inputLines = splitLines(input);
+    return std::accumulate(inputLines.begin(),
+                    inputLines.end(), 
+                    0,
+                    [](auto result, const auto& currentLine){
+                        return result + reconstructCalibrationDocumentLine(currentLine);
+                    });
 }
--- cpp/day1/tests/testMain.cpp
+++ cpp/day1/tests/testMain.cpp
@@ -4,8 +4,50 @@
 
 using namespace ::testing;
 
-TEST(AdventOfCodeDay1Test, verify_sumOfNumbersInLines)
+TEST(AdventOfCodeDay1Test, DISABLED_verify_sumOfNumbersInLines)
 {
     constexpr auto input = "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet";
     EXPECT_EQ(142, reconstructCalibrationDocument(input));
+}
+
+TEST(AdventOfCodeDay1Test, verify_1Line)
+{
+    constexpr auto input{"11"};
+    EXPECT_EQ(11, reconstructCalibrationDocument(input));
+}
+
+TEST(AdventOfCodeDay1Test, verify_1LineWithChars)
+{
+    constexpr auto input{"1abc2"};
+    EXPECT_EQ(12, reconstructCalibrationDocument(input));
+}
+
+TEST(AdventOfCodeDay1Test, verify_1LineWithMultipleNumbers)
+{
+    constexpr auto input{"1a5c2"};
+    EXPECT_EQ(12, reconstructCalibrationDocument(input));
+}
+
+TEST(AdventOfCodeDay1Test, verify_1LineWithOnlyMultipleNumbers)
+{
+    constexpr auto input{"123456"};
+    EXPECT_EQ(16, reconstructCalibrationDocument(input));
+}
+
+TEST(AdventOfCodeDay1Test, verify_1LineWithOnly1Number)
+{
+    constexpr auto input{"a1c"};
+    EXPECT_EQ(11, reconstructCalibrationDocument(input));
+}
+
+TEST(AdventOfCodeDay1Test, verify_1LineOnly1Digit)
+{
+    constexpr auto input{"1"};
+    EXPECT_EQ(11, reconstructCalibrationDocument(input));
+}
+
+TEST(AdventOfCodeDay1Test, verify_2Line)
+{
+    constexpr auto input{"ab1c2\ngasd26"};
+    EXPECT_EQ(38, reconstructCalibrationDocument(input));
 }
