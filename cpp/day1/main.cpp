605cf8640814af6567b036b29132e6287c30b84a
xiaoming <mail@zyming.me>
2023-12-01 20:02:56 +0100

    Solution for Day1 problem2 is now up

--- cpp/day1/main.cpp
+++ cpp/day1/main.cpp
@@ -9,9 +9,16 @@ void solveProblem1(const std::string& input)
     std::cout << "Solution for problem 1: " << results << std::endl;
 }
 
+void solveProblem2(const std::string& input)
+{
+    const auto results = reconstructCalibrationDocumentV2(input);
+    std::cout << "Solution for problem 2: " << results << std::endl;
+}
+
 int main()
 {
     const auto input = readInput();
     solveProblem1(input);
+    solveProblem2(input);
     return 0;
 }
