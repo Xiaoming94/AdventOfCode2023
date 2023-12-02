254f102d7b1fb812968381893fb57c9039fbd541
xiaoming <mail@zyming.me>
2023-12-03 00:31:26 +0100

    Add tests for day1 problems

--- rust/tests/day2_tests.rs
+++ rust/tests/day2_tests.rs
@@ -1,6 +1,8 @@
 use googletest::prelude::*;
 use advent_of_code::day2;
 
+const NO_VALID_GAMES: Vec<u32> = Vec::<u32>::new();
+
 #[googletest::test]
 #[ignore]
 fn day2_problem1_acceptance_test()
@@ -19,11 +21,73 @@ fn day2_problem1_acceptance_test()
 }
 
 #[googletest::test]
-fn day2_problem1_game_validround()
+fn day2_problem1_1game_1bluecube()
+{
+    let input = "Game 1: 1 blue";
+    let cube_game = day2::Game::new(12,13,14);
+
+    let results = day2::check_possible_games(input.to_string(), cube_game);
+    expect_that!(results, eq(vec![1]));
+}
+
+#[googletest::test]
+fn day2_problem1_1game_validround()
 {
     let input = "Game 1: 3 blue, 1 red, 1 green";
     let cube_game = day2::Game::new(12,13,14);
 
     let results = day2::check_possible_games(input.to_string(), cube_game);
-    expect_that!(results, eq(Vec::from([1])));
+    expect_that!(results, eq(vec![1]));
+}
+
+#[googletest::test]
+fn day2_problem1_1game_invalidround()
+{
+    let input = "Game 1: 3 blue, 14 red, 1 green";
+    let cube_game = day2::Game::new(12,13,14);
+
+    let results = day2::check_possible_games(input.to_string(), cube_game);
+    expect_that!(results, eq(NO_VALID_GAMES));
+}
+
+#[googletest::test]
+fn day2_problem1_1game_2validrounds()
+{
+    let input = "Game 1: 3 blue, 1 red, 1 green; 5 red, 4 green";
+    let cube_game = day2::Game::new(12,13,14);
+
+    let results = day2::check_possible_games(input.to_string(), cube_game);
+    expect_that!(results, eq(vec![1]));
+}
+
+#[googletest::test]
+fn day2_problem1_1game_1valid_1invalidrounds()
+{
+    let input = "Game 1: 3 blue, 1 red, 1 green; 14 red, 4 green";
+    let cube_game = day2::Game::new(12,13,14);
+
+    let results = day2::check_possible_games(input.to_string(), cube_game);
+    expect_that!(results, eq(NO_VALID_GAMES));
+}
+
+#[googletest::test]
+fn day2_problem1_2games_validrounds()
+{
+    let input = "Game 1: 3 blue, 1 red, 1 green; 5 red, 4 green\n\
+                    Game 2: 5 blue, 10 red";
+    let cube_game = day2::Game::new(12,13,14);
+
+    let results = day2::check_possible_games(input.to_string(), cube_game);
+    expect_that!(results, eq(vec![1,2]));
+}
+
+#[googletest::test]
+fn day2_problem1_2games_1invalidgame()
+{
+    let input = "Game 1: 3 blue, 1 red, 1 green; 5 red, 4 green\n\
+                    Game 2: 5 blue, 20 red";
+    let cube_game = day2::Game::new(12,13,14);
+
+    let results = day2::check_possible_games(input.to_string(), cube_game);
+    expect_that!(results, eq(vec![1,2]));
 }
