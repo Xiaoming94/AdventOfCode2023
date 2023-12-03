ea16ba2c4dc60126799790f24d41146202e3abcf
xiaoming <mail@zyming.me>
2023-12-03 10:26:20 +0100

    Separate read_input into a utility file

--- rust/src/day2_main.rs
+++ rust/src/day2_main.rs
@@ -1,15 +1,7 @@
-use advent_of_code::day2;
-use std::io::BufRead;
+use advent_of_code::{day2, utils::read_input};
 
 fn main() -> Result<(), std::io::Error> {
-    let input_io = std::io::stdin();
-    let mut input_lines = String::new();
-    for line in input_io.lock().lines() {
-        input_lines.push_str(line.unwrap().as_str());
-        input_lines.push_str("\n");
-    }
-    let mut input_lines = input_lines.chars();
-    input_lines.next_back();
+    let input_lines = read_input();
     let cube_game = day2::Game::new(12u32,13u32,14u32);
     let results = day2::check_possible_games(input_lines.as_str(), &cube_game);
     println!("Valid games: {:?}", results);
---
+++ rust/src/utils/mod.rs
@@ -0,0 +1,12 @@
+use std::io::BufRead;
+
+pub fn read_input() -> String
+{
+    let input_io = std::io::stdin();
+    let mut input_lines: Vec<String> = Vec::<String>::new();
+    for line in input_io.lock().lines() {
+        input_lines.push(line.unwrap());
+    }
+    
+    input_lines.join("\n")
+}
