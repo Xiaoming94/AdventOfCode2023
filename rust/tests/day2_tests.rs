a95b39b02c9bce45e044f62a6e81b318c5ca1a02
xiaoming <mail@zyming.me>
2023-12-03 11:00:05 +0100

    Reorganize tests in day2_tests.rs

--- rust/tests/day2_tests.rs
+++ rust/tests/day2_tests.rs
@@ -34,74 +34,78 @@ mod day2_acceptance_tests {
     }
 }
 
-#[googletest::test]
-fn day2_problem1_1game_1bluecube()
-{
-    let input = "Game 1: 1 blue";
-    let cube_game = day2::Game::new(12,13,14);
+#[cfg(test)]
+mod tc_tests{
+    use super::*;
+    #[googletest::test]
+    fn tc_1game_1bluecube()
+    {
+        let input = "Game 1: 1 blue";
+        let cube_game = day2::Game::new(12,13,14);
 
-    let results = day2::check_possible_games(input, &cube_game);
-    expect_that!(results, eq(vec![1]));
-}
+        let results = day2::check_possible_games(input, &cube_game);
+        expect_that!(results, eq(vec![1]));
+    }
 
-#[googletest::test]
-fn day2_problem1_1game_validround()
-{
-    let input = "Game 1: 3 blue, 1 red, 1 green";
-    let cube_game = day2::Game::new(12,13,14);
+    #[googletest::test]
+    fn tc_1game_validround()
+    {
+        let input = "Game 1: 3 blue, 1 red, 1 green";
+        let cube_game = day2::Game::new(12,13,14);
 
-    let results = day2::check_possible_games(input, &cube_game);
-    expect_that!(results, eq(vec![1]));
-}
+        let results = day2::check_possible_games(input, &cube_game);
+        expect_that!(results, eq(vec![1]));
+    }
 
-#[googletest::test]
-fn day2_problem1_1game_invalidround()
-{
-    let input = "Game 1: 3 blue, 14 red, 1 green";
-    let cube_game = day2::Game::new(12,13,14);
+    #[googletest::test]
+    fn tc_1game_invalidround()
+    {
+        let input = "Game 1: 3 blue, 14 red, 1 green";
+        let cube_game = day2::Game::new(12,13,14);
 
-    let results = day2::check_possible_games(input, &cube_game);
-    expect_that!(results, eq(NO_VALID_GAMES));
-}
+        let results = day2::check_possible_games(input, &cube_game);
+        expect_that!(results, eq(NO_VALID_GAMES));
+    }
 
-#[googletest::test]
-fn day2_problem1_1game_2validrounds()
-{
-    let input = "Game 1: 3 blue, 1 red, 1 green; 5 red, 4 green";
-    let cube_game = day2::Game::new(12,13,14);
+    #[googletest::test]
+    fn tc_1game_2validrounds()
+    {
+        let input = "Game 1: 3 blue, 1 red, 1 green; 5 red, 4 green";
+        let cube_game = day2::Game::new(12,13,14);
 
-    let results = day2::check_possible_games(input, &cube_game);
-    expect_that!(results, eq(vec![1]));
-}
+        let results = day2::check_possible_games(input, &cube_game);
+        expect_that!(results, eq(vec![1]));
+    }
 
-#[googletest::test]
-fn day2_problem1_1game_1valid_1invalidrounds()
-{
-    let input = "Game 1: 3 blue, 1 red, 1 green; 14 red, 4 green";
-    let cube_game = day2::Game::new(12,13,14);
+    #[googletest::test]
+    fn tc_1game_1valid_1invalidrounds()
+    {
+        let input = "Game 1: 3 blue, 1 red, 1 green; 14 red, 4 green";
+        let cube_game = day2::Game::new(12,13,14);
 
-    let results = day2::check_possible_games(input, &cube_game);
-    expect_that!(results, eq(NO_VALID_GAMES));
-}
+        let results = day2::check_possible_games(input, &cube_game);
+        expect_that!(results, eq(NO_VALID_GAMES));
+    }
 
-#[googletest::test]
-fn day2_problem1_2games_validrounds()
-{
-    let input = "Game 1: 3 blue, 1 red, 1 green; 5 red, 4 green\n\
-                    Game 2: 5 blue, 10 red";
-    let cube_game = day2::Game::new(12,13,14);
+    #[googletest::test]
+    fn tc_2games_validrounds()
+    {
+        let input = "Game 1: 3 blue, 1 red, 1 green; 5 red, 4 green\n\
+                        Game 2: 5 blue, 10 red";
+        let cube_game = day2::Game::new(12,13,14);
 
-    let results = day2::check_possible_games(input, &cube_game);
-    expect_that!(results, eq(vec![1,2]));
-}
+        let results = day2::check_possible_games(input, &cube_game);
+        expect_that!(results, eq(vec![1,2]));
+    }
 
-#[googletest::test]
-fn day2_problem1_2games_1invalidgame()
-{
-    let input = "Game 1: 3 blue, 1 red, 1 green; 5 red, 4 green\n\
-                    Game 2: 5 blue, 20 red";
-    let cube_game = day2::Game::new(12,13,14);
+    #[googletest::test]
+    fn tc_2games_1invalidgame()
+    {
+        let input = "Game 1: 3 blue, 1 red, 1 green; 5 red, 4 green\n\
+                        Game 2: 5 blue, 20 red";
+        let cube_game = day2::Game::new(12,13,14);
 
-    let results = day2::check_possible_games(input, &cube_game);
-    expect_that!(results, eq(vec![1]));
-}
+        let results = day2::check_possible_games(input, &cube_game);
+        expect_that!(results, eq(vec![1]));
+    }
+} 
