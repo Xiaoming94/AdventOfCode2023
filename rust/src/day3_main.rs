4ccb1f7f75b09a23cc2c542e79d17769ff59fd6b
xiaoming <mail@zyming.me>
2023-12-09 21:56:43 +0100

    Complete day3 finally

--- rust/src/day3_main.rs
+++ rust/src/day3_main.rs
@@ -7,5 +7,9 @@ fn main() -> Result<(), std::io::Error>
     let results = day3::find_partnumbers(&schematic_input);
     println!("Found schematic numbers {:?}", results);
     println!("Resulting sum {:?}", results.iter().sum::<u32>());
+
+    let results_problem2 = day3::find_gear_ratios(&schematic_input);
+    println!("Found gear ratios {:?}", results_problem2);
+    println!("Resulting sum {:?}", results_problem2.iter().sum::<u32>());
     Ok(())
 }
