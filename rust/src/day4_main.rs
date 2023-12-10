69d5f05c1257d3eb75e48892c09cf4d94caff3c6
xiaoming <mail@zyming.me>
2023-12-10 19:10:47 +0100

    Formatting

--- rust/src/day4_main.rs
+++ rust/src/day4_main.rs
@@ -1,15 +1,12 @@
-use advent_of_code::utils;
 use advent_of_code::day4;
+use advent_of_code::utils;
 
 fn main() -> Result<(), std::io::Error> {
     let card_input_str = utils::read_input();
     let matching_nums = day4::find_winning_card_scores(&card_input_str);
     println!("===== Results =====");
     println!("{:?}", matching_nums);
-    let scores: u32 = matching_nums
-        .values()
-        .map(day4::calc_score)
-        .sum();
+    let scores: u32 = matching_nums.values().map(day4::calc_score).sum();
     println!("Scores: {}", scores);
     Ok(())
 }
