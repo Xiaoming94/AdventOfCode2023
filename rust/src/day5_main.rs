208c32ee1fc1dfd339cd75aba3e76bec2a3b1ca1
xiaoming <mail@zyming.me>
2023-12-22 10:06:43 +0100

    Complete executable solution for problem 5

--- rust/src/day5_main.rs
+++ rust/src/day5_main.rs
@@ -4,7 +4,9 @@ use advent_of_code::utils;
 fn main() -> Result<(), std::io::Error> {
     println!("======= RESULTS FOR DAY5 =======");
     let input_almanac = utils::read_input();
-    let smallest_location = day5::find_lowest_location(input_almanac.as_str());
-    println!("Lowest seed location: {}", smallest_location);
+    let smallest_location_1 = day5::find_lowest_location(input_almanac.as_str());
+    let smallest_location_2 = day5::find_lowest_location_v2(input_almanac.as_str());
+    println!("== PROBLEM 1 Lowest location number: {}", smallest_location_1);
+    println!("== PROBLEM 2 Lowest location number: {}", smallest_location_2);
     Ok(())
 }
