fbb1be1ea916ac7c076843c7b8409a295db24834
Xiaoming <mail@zyming.me>
2023-12-22 11:31:23 +0100

    Add boilerplate for day6 problems

---
+++ rust/src/day6/mod.rs
@@ -0,0 +1,3 @@
+pub fn find_winning_solutions(race_data: &str) -> Vec<u32> {
+    Vec::new()
+}
---
+++ rust/src/day6_main.rs
@@ -0,0 +1,6 @@
+use advent_of_code::day6;
+
+fn main() -> Result<(), std::io::Error> {
+    println!("Hello day6");
+    Ok(())
+}
