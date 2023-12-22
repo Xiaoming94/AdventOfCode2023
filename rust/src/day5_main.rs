95a19cd208396c22d48bf309b2e491943aef72df
Xiaoming <mail@zyming.me>
2023-12-22 11:33:55 +0100

    Formatting

--- rust/src/day5_main.rs
+++ rust/src/day5_main.rs
@@ -6,7 +6,13 @@ fn main() -> Result<(), std::io::Error> {
     let input_almanac = utils::read_input();
     let smallest_location_1 = day5::find_lowest_location(input_almanac.as_str());
     let smallest_location_2 = day5::find_lowest_location_v2(input_almanac.as_str());
-    println!("== PROBLEM 1 Lowest location number: {}", smallest_location_1);
-    println!("== PROBLEM 2 Lowest location number: {}", smallest_location_2);
+    println!(
+        "== PROBLEM 1 Lowest location number: {}",
+        smallest_location_1
+    );
+    println!(
+        "== PROBLEM 2 Lowest location number: {}",
+        smallest_location_2
+    );
     Ok(())
 }
--- rust/tests/day6_tests.rs
+++ rust/tests/day6_tests.rs
@@ -9,11 +9,11 @@ mod acceptance_tests {
     const INPUT: &str = "Time:      7  15   30\nDistance:  9  40  200";
     #[googletest::test]
     #[ignore]
-    fn problem1(){
+    fn problem1() {
         let solutions = day6::find_winning_solutions(INPUT);
         let expected_solutions = vec![4u32, 8u32, 9u32];
         expect_that!(solutions, eq(expected_solutions));
-        
+
         let result: u32 = solutions.iter().product();
         expect_that!(result, eq(288));
     }
