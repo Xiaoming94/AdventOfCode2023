f9ae7ea37e4cf5bd8011e0f6fb074a1443413741
xiaoming <mail@zyming.me>
2023-12-22 23:36:20 +0100

    Add tests for day 7 problem 2 with jokers

--- rust/tests/day7_tests.rs
+++ rust/tests/day7_tests.rs
@@ -17,6 +17,12 @@ mod day7_acceptance_test {
         let result = day7::compute_hands_bid_value(INPUT);
         expect_that!(result, eq(6440));
     }
+    
+    #[googletest::test]
+    fn problem2() {
+        let result = day7::compute_hands_bid_jokers(INPUT);
+        expect_that!(result, eq(5905));
+    }
 }
 
 #[cfg(test)]
@@ -40,7 +46,7 @@ mod day7_problem1_tests {
     #[googletest::test]
     fn tc_2hands_1stbigger() {
         let input = "KKKJJ 15\n\
-                           QQQ12 12";
+                     QQQ12 12";
         let result = day7::compute_hands_bid_value(input);
         expect_that!(result, eq(42));
     }
@@ -48,7 +54,7 @@ mod day7_problem1_tests {
     #[googletest::test]
     fn tc_2hands_2ndbigger() {
         let input = "KKJJ2 15\n\
-                           QQQ77 12";
+                     QQQ77 12";
         let result = day7::compute_hands_bid_value(input);
         expect_that!(result, eq(39));
     }
@@ -56,8 +62,51 @@ mod day7_problem1_tests {
     #[googletest::test]
     fn tc_2hands_one_underordered() {
         let input = "Q2JJJ 15\n\
-                           QTQQT 12";
+                     QTQQT 12";
         let result = day7::compute_hands_bid_value(input);
         expect_that!(result, eq(39));
+    }
+}
+
+#[cfg(test)]
+mod day7_problem2_tests {
+    use super::*;
+
+    #[googletest::test]
+    fn tc_1hand() {
+        let input = "31234 12";
+        let result = day7::compute_hands_bid_jokers(input);
+        expect_that!(result, eq(12));
+    }
+
+    #[googletest::test]
+    fn tc_1hand2() {
+        let input = "KKKJJ 15";
+        let result = day7::compute_hands_bid_jokers(input);
+        expect_that!(result, eq(15));
+    }
+
+    #[googletest::test]
+    fn tc_2hands_1stbigger() {
+        let input = "KKKJJ 15\n\
+                     QQQ12 12";
+        let result = day7::compute_hands_bid_jokers(input);
+        expect_that!(result, eq(42));
+    }
+
+    #[googletest::test]
+    fn tc_2hands_2ndbigger() {
+        let input = "KKJJ2 15\n\
+                     QQQ77 12";
+        let result = day7::compute_hands_bid_jokers(input);
+        expect_that!(result, eq(42));
+    }
+
+    #[googletest::test]
+    fn tc_2hands_one_underordered() {
+        let input = "Q2JJJ 15\n\
+                     QTQQT 12";
+        let result = day7::compute_hands_bid_jokers(input);
+        expect_that!(result, eq(42));
     }
 }
