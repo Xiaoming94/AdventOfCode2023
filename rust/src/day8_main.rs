d87c2bd11127bc5227a4fb0cdd860a1251bb37f3
Xiaoming94 <mail@zyming.me>
2023-12-24 17:08:12 +0100

    Complete day8 problem1

--- rust/src/day8_main.rs
+++ rust/src/day8_main.rs
@@ -1,6 +1,13 @@
-use advent_of_code::day8;
+use advent_of_code::{day8, utils};
 
 fn main() -> Result<(), std::io::Error> {
     println!("======= ADVENT OF CODE DAY 8 =======");
+    let graph_input = utils::read_input();
+    let results = day8::find_path_through_network(graph_input.as_str());
+    println!(
+        "PROBLEM1 == number of steps to complete the walk is: {}",
+        results
+    );
+
     Ok(())
 }
